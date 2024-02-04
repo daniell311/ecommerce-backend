f40b02310ab883855253dda28f6ca626a5870ebc
daniell0311 <daniell3119977@gmail.com>
2024-02-05 00:28:33 +0700

    move function for looking email existing to utils email validator

--- models/AuthModel.js
+++ models/AuthModel.js
@@ -1,23 +1,6 @@
 import db from '../database/connection.js';
 
 class AuthModel{
-    async isEmailExist(){
-        try {
-            const sql = `select email from users where email = ${ email } `;
-            const myPromise = () => {
-                return new Promise((resolve, reject) => {
-                    db.query(sql, (err, fields) => {
-                        err ? reject(err) : resolve(fields.rows);
-                    });
-                });
-            }; 
-            let result = await(myPromise());
-            return result;
-        } catch (error) {
-            return error;
-        }
-    }
-
     async createUser(data){
         try {
             const sql = `INSERT INTO auth.a_users (username, fullname, email, password, roleid, softdelete, isAktif, salt) VALUES ('${data.username}', '${data.fullname}', '${data.email}', '${data.password}', '${data.roleid}', '${data.softdelete}', '${data.isAktif}', '${data.salt}')`;
@@ -25,7 +8,6 @@ class AuthModel{
                 return new Promise((resolve, reject) => {
                     db.query(sql, (err, fields) => {
                         err ? reject(err) : resolve(fields);
-                        console.log({fi : fields});
                     })
                 });
             }
