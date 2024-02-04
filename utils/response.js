9f3a30a7440124da1bdaf1da954450d09c5489c0
daniell0311 <daniell3119977@gmail.com>
2024-02-04 16:03:58 +0700

    response template

---
+++ utils/response.js
@@ -0,0 +1,32 @@
+const getPagingData = (data, page, limit) => {
+    const totalItems = data;
+    const currentPage = page ? +page : 0;
+    const totalPages = Math.ceil(totalItems / limit);
+    const perPages = limit;
+    const next = currentPage == totalPages ? "" : currentPage + 1;
+    const previous = currentPage == 1 ? "" : currentPage - 1;
+  
+    return { totalItems, totalPages, perPages, currentPage, previous, next };
+  };
+  
+  // Format response Payload/Output Data API
+  export const response = (statusCode, data, msg, res, totalRows) => {
+    res.status(statusCode).json({
+      status_code: statusCode,
+      message: msg,
+      totalRows: totalRows,
+      payload: data,
+    });
+  };
+  
+  export const responsePaginate = (statusCode, data, msg, res, limit, totalRows, page
+  ) => {
+    res.status(statusCode).json({
+      status_code: statusCode,
+      message: msg,
+      totalRows: totalRows,
+      payload: data,
+      paging: getPagingData(totalRows, page, limit),
+    });
+  };
+  
