fd14b1fddac07671123a8e71b1fa386a623df19e
daniell0311 <daniell3119977@gmail.com>
2024-02-04 16:04:42 +0700

    utils for generate jwt token

---
+++ utils/generateToken.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+const env = dotenv.config().parsed
+
+export const generateAccessToken = async (payload) => {
+    return jwt.sign(payload,
+        env.ACCESS_TOKEN,
+        { expiresIn : env.ACCESS_TOKEN_EXPIRATION }
+        )
+}
+
+export const generateRefreshToken = async (payload) => {
+    return jwt.sign(payload,
+        env.REFRESH_TOKEN,
+        { expiresIn : env.REFRESH_TOKEN_EXPIRATION }
+        )
+}
