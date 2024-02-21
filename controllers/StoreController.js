2babf5a874e49c42b60b78a60b8d37ff5e1187d5
daniell0311 <daniell3119977@gmail.com>
2024-02-22 00:16:16 +0700

    Add=> add email and its validation, and add phone number validations

--- controllers/StoreController.js
+++ controllers/StoreController.js
@@ -1,23 +1,32 @@
 import queryHelper from "../utils/queryHelper.js";
 import { response, responsePaginate, responseError } from "../utils/response.js";
+import { regexEmailValidator } from "../utils/emailValidator.js";
 
 class StoreController {
     async createStore(req, res){
         try {
-            const { storeName, storeDescription, noHp, address, storeLink} = req.body;
+            const { storeName, storeDescription, phone, address, storeLink, email} = req.body;
     
             if(!storeName) { return responseError(404, "Store Name is required!", res ) };
             if(!storeDescription) { return responseError(404, "Store Description is required!", res ) };
-            
+            if(!regexEmailValidator(email)) { return responseError(404, "Emaill is not Valid", res ) };
+
             const isStoreNameExist = await queryHelper.isExist('store', 'st_store', 'storename', ` storename = '${ storeName }'`);
+            const isPhoneExist = await queryHelper.isExist('store', 'st_store', 'phone', ` phone = '${ phone }'`);
+            const isEmailExist = await queryHelper.isExist('store', 'st_store', 'email', ` email = '${ email }'`);
+            
             if(isStoreNameExist) { return responseError(404, "Store Name already Exist!", res ) };
+            if(isPhoneExist) { return responseError(404, "Phone Number already used!", res ) }; 
+            if(isEmailExist) { return responseError(404, "Email already used!", res ) }; 
+            
             const data = {
                 storename : storeName,
                 userid : req.jsonwebtoken.userid,
                 storedescription : storeDescription,
-                nohp : noHp,
+                phone : phone,
                 address : address,
-                storelink : storeLink
+                storelink : storeLink,
+                email : email
             };
             const result = await queryHelper.insertData('store', 'st_store', data);
             if(result){
