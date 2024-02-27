dcf75c1f977c09e0c0d666c6baeaabc548954e13
daniell0311 <daniell3119977@gmail.com>
2024-02-27 21:43:49 +0700

    chore: add static schema and table name for reusable query

---
+++ models/ProductModel.js
@@ -0,0 +1,11 @@
+import queryHelper from "../utils/queryHelper.js";
+
+export const schema = 'product';
+export const table = 'p_product';
+export const schemaTable = schema+'.'+table;
+
+class ProductModel{
+
+}
+
+export default new ProductModel();
---
+++ models/StoreModel.js
@@ -0,0 +1,11 @@
+import queryHelper from "../utils/queryHelper.js";
+
+export const schema = 'store';
+export const table = 'st_store';
+export const schemaTable = schema+'.'+table;
+
+class StoreModel{
+
+}
+
+export default new StoreModel();
