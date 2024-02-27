85ec0c8fb5e083f30bdbdd287a934ecd95e15ecd
daniell0311 <daniell3119977@gmail.com>
2024-02-27 21:45:31 +0700

    chore: create static variable for table and schema name

--- models/AuthModel.js
+++ models/AuthModel.js
@@ -1,22 +1,11 @@
 import db from '../database/connection.js';
 
+export const schema = 'auth';
+export const table = 'a_users';
+export const schemaTable = schema+'.'+table;
+
 class AuthModel{
-    async createUser(data){
-        try {
-            const sql = `INSERT INTO auth.a_users (username, fullname, email, password, roleid, softdelete, isAktif, salt) VALUES ('${data.username}', '${data.fullname}', '${data.email}', '${data.password}', '${data.roleid}', '${data.softdelete}', '${data.isAktif}', '${data.salt}')`;
-            let myPromise = () => {
-                return new Promise((resolve, reject) => {
-                    db.query(sql, (err, fields) => {
-                        err ? reject(err) : resolve(fields);
-                    })
-                });
-            }
-            let result = await (myPromise());
-            return result;
-        } catch (error) {
-            return error;
-        }
-    }
+   
 }
 
 export default new AuthModel();
