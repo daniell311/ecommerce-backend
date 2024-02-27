9ac070c785666df5b9766be1c8a5c947ca0b626d
daniell0311 <daniell3119977@gmail.com>
2024-02-27 21:47:33 +0700

    chore: adjust to new utils queryHelper params, does not required table and schema params, now merged those 2 into schemaTable variable that declared in each model later on

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,13 +1,13 @@
 import bcrypt from "bcrypt";
-import {regexEmailValidator} from "../utils/emailValidator.js";
+import { regexEmailValidator } from "../utils/emailValidator.js";
 import { generateAccessToken, generateRefreshToken } from "../utils/generateToken.js";
 import authModel from "../models/AuthModel.js";
 import queryHelper from "../utils/queryHelper.js";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import { schemaTable } from "../models/AuthModel.js";
 
 const env = dotenv.config().parsed;
-
 
 class AuthController{
 
@@ -20,9 +20,9 @@ class AuthController{
             if(!password) { throw { code : 404, message : 'Password is Required '} };
             if(!regexEmailValidator(email)) { throw { code : 404, message : 'Emaill is not Valid' }};
 
-            const emailExist = await queryHelper.isExist('auth', 'a_users', 'email', `email = '${email}'`);
+            const emailExist = await queryHelper.isExist(schemaTable, 'email', `email = '${email}'`);
             if(emailExist) { throw { code : 404, message : 'Email Already Exist' } };
-            const usernameExist = await queryHelper.isExist('auth', 'a_users', 'username', `username = '${username}'`);
+            const usernameExist = await queryHelper.isExist(schemaTable, 'username', `username = '${username}'`);
             if(usernameExist) { throw { code : 404, message : 'Username Already Exist' } };
 
             const salt = await bcrypt.genSalt(15);
@@ -37,9 +37,9 @@ class AuthController{
                 isAktif : 1,
                 salt : salt,
             }
-            const result = await queryHelper.insertData('auth', 'a_users', data);
+            const result = await queryHelper.insertData(schemaTable, data);
             if(result.rowCount){
-                const userId = await queryHelper.findOne('auth', 'a_users', 'userid', ` username = '${ username }'`);
+                const userId = await queryHelper.findOne(schemaTable, 'userid', ` username = '${ username }'`);
                 // payload user data used as a session when user login
                 let payload = { 
                     userid: userId, 
@@ -77,7 +77,7 @@ class AuthController{
             if(!username) { throw { code : 400, message : "Please enter a Username" } }
             if(!password) { throw { code : 400, message : "Please enter a Password" } }
 
-            const user = await queryHelper.getRow('auth', 'a_users', `username = '${username}'`)
+            const user = await queryHelper.getRow(schemaTable, `username = '${username}'`)
             if( user == undefined) { throw { code :404, message: "USER NOT FOUND" } }
             const isPasswordValid = await bcrypt.compareSync(password, user.password)
             if(!isPasswordValid) { throw { code :404, message: "INVALID PASSWORD"}}
